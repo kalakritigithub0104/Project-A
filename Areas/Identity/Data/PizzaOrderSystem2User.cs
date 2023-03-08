52589258d835a08c5a8b482fefb3ab30b8cf3e86
kalakritigithub0104 <113574700+kalakritigithub0104@users.noreply.github.com>
2023-03-08 17:02:56 +0530

    Add files via upload

---
+++ Areas/Identity/Data/PizzaOrderSystem2User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PizzaOrderSystem2.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the PizzaOrderSystem2User class
+public class PizzaOrderSystem2User : IdentityUser
+{
+}
+
