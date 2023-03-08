ef8c38268d158dce7ea26040449cd2efb1591f53
kalakritigithub0104 <113574700+kalakritigithub0104@users.noreply.github.com>
2023-03-08 17:05:18 +0530

    Add files via upload

---
+++ Data/PizzaOrderSystem2Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrderSystem2.Models;
+
+namespace PizzaOrderSystem2.Data
+{
+    public class PizzaOrderSystem2Context : IdentityDbContext
+    {
+        public PizzaOrderSystem2Context (DbContextOptions<PizzaOrderSystem2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PizzaOrderSystem2.Models.Pizza> Pizza { get; set; } = default!;
+
+        public DbSet<PizzaOrderSystem2.Models.Order> Order { get; set; } = default!;
+
+        public DbSet<PizzaOrderSystem2.Models.Status> Status { get; set; } = default!;
+    }
+}
